de293d4e541befc4a0f9e605844eb1a0e3b6f657
yash jain <64656456+yajain17@users.noreply.github.com>
2023-09-28 13:41:07 +0530

    Add files via upload

---
+++ real-estate-app-master2/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Real Estate Analytics App 👋  ")
+st.write("This App will help you to make better decision while Purchasing Properties. We have three options :\n\n \n 1.Price Predictor\n\n 2.Analysis App\n\n 3.Recmommend Appartments")
+st.write("Created by ; Yash Jain")
+st.sidebar.success("Select a demo above.")
